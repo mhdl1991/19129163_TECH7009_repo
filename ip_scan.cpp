fb3fca156e3aba8598a21482b656811b5c947b26
mhdl1991 <mohammad.ali1991@live.com>
2023-06-29 16:32:22 +0100

    hm, what to do, what to do...

---
+++ ip_scan.cpp
@@ -0,0 +1,14 @@
+//IP scanning with nmap
+
+// properties: "-sS -Pn -n -T5 --min-hostgroup=2000 --max-rtt-timeout=500ms --min-rate=10000"
+// -sS SYN scan
+
+// openssl s_client -showcerts -connect some_server:server_port
+
+
+int main() {
+	
+	
+	
+	return 0;
+}
