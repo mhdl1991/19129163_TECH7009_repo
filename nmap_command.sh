e61a30ed4e5e7f3e439584bf84a36e0d63ec8aba
mhdl1991 <mohammad.ali1991@live.com>
2023-07-01 15:39:10 +0100

    Nmap command testing

--- nmap_command.sh
+++ nmap_command.sh
@@ -2,10 +2,13 @@
 
 echo "Running nmap"
 
-nmap -sS -p 22,443 -n -T5 --script ssl-enum-ciphers.nse -D RND:10 -oT nmap_out_txt.txt --min-hostgroup=2000 --max-rtt-timeout=500ms --min-rate=10000 scanme.nmap.org
+nmap -sS -p 22,443 -n -T5 --script ssl-enum-ciphers.nse -D RND:10 -oX nmap_out.xml --min-hostgroup=2000 --max-rtt-timeout=500ms --min-rate=10000 "192.168.10.0/24"
+
+#scanme.nmap.org
 
 echo "This is where certificate acquisition should take place using perhaps ssl"
 
+#echo | openssl s_client -servername 
 
 echo "End of bash script" 
 
