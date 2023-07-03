dfe12176e00c8f6623c60ca706f860417be5487f
mhdl1991 <mohammad.ali1991@live.com>
2023-07-03 15:16:11 +0100

    I think we're getting somewhere?

--- nmap_command.sh
+++ nmap_command.sh
@@ -2,9 +2,11 @@
 
 echo "Running nmap"
 
-nmap -sV -p 22,443 -n -T5 --script ssl-enum-ciphers -D RND:10 -oX nmap_out.xml --min-hostgroup=2000 --max-rtt-timeout=500ms --min-rate=10000 "10.0.2.3"
+nmap -sV -p 22,443, -n -T5 --script ssl-enum-ciphers -D RND:10 -oX nmap_out.xml "www.google.com"
 
-#scanme.nmap.org
+
+#aggressive version
+#nmap -sV -p 22,443 -n -T5 --script ssl-enum-ciphers -D RND:10 -oX nmap_out.xml --min-hostgroup=2000 --max-rtt-timeout=500ms --min-rate=10000 "scanme.nmap.org"
 
 echo "This is where certificate acquisition should take place using perhaps ssl"
 
